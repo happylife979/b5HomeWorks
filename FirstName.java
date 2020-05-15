6fa4d09fa0e0a2daf48203b0743391bf20e7edd1
happylife979 <65377757+happylife979@users.noreply.github.com>
2020-05-14 21:46:04 -0400

    Uploading second homework may142020

---
+++ FirstName.java
@@ -0,0 +1,10 @@
+public class FirstName {
+	
+    public static void main(String[] args){
+        String name = "Aiza";
+        System.out.println(name);
+        name = "David";
+        System.out.println(name);
+    }
+
+}
